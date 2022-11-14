cd3dfe068303d1be3b5417ea255a2ce0b4b5c4c8
박건웅 <pkw132@naver.com>
2022-11-14 17:10:45 +0900

    up

---
+++ JavaScript/new.js
@@ -0,0 +1,17 @@
+
+
+var family = {
+    "boy":"gunwoong",
+    "girl":"harim"
+  };
+
+
+var i=0;
+
+document.write("남자: ",family.boy,"<br>");
+
+while(i<3)
+{
+document.write(family[i],'<br>');
+i+=1;
+}  
